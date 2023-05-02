27fc8c16d850d4317e297592c27cb1a1c428cca1
Rohit Nandi <rohitnandi12@gmail.com>
2023-05-02 23:41:41 +0530

    project setup and root controller

--- src/main/java/com/tinyurl/tinyurl/TinyUrlApplication.java
+++ src/main/java/com/tinyurl/tinyurl/TinyUrlApplication.java
@@ -1,20 +1,18 @@
 package com.tinyurl.tinyurl;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
 public class TinyUrlApplication {
-	private static final Logger logger=LoggerFactory.getLogger(TinyUrlApplication.class);
+    private static final Logger logger = LoggerFactory.getLogger(TinyUrlApplication.class);
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication.run(TinyUrlApplication.class, args);
-		logger.info("Server started...");
-	}
+        SpringApplication.run(TinyUrlApplication.class, args);
+        logger.info("Server started...");
+    }
 
 }
--- src/main/java/com/tinyurl/tinyurl/controller/TinyUrlController.java
+++ src/main/java/com/tinyurl/tinyurl/controller/TinyUrlController.java
@@ -1,17 +1,18 @@
 package com.tinyurl.tinyurl.controller;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
-
-public class TinyUrlController
-{
-   // private static final Logger logger=LoggerFactory.getLogger(TinyUrlController.class);
+public class TinyUrlController {
+    private static final Logger logger = LoggerFactory.getLogger(TinyUrlController.class);
     @GetMapping("/")
-    public String getGreeting(){
+    public String greet() {
+        logger.info("Inside root '/' controller");
         return "Namaste!";
     }
+
 }
+
